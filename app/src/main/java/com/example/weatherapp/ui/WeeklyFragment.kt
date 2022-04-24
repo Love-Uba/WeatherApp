45e9bb256d8dd0c1e007eeb48d6a9303679fbf9b
decagon <loveubadinma@gmail.com>
2022-04-24 18:36:04 +0100

    it now works from one vm

--- app/src/main/java/com/example/weatherapp/ui/WeeklyFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/WeeklyFragment.kt
@@ -15,6 +15,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
@@ -24,6 +25,7 @@ import com.example.weatherapp.ui.adapter.WeatherForDaysAdapter
 import com.example.weatherapp.utils.format
 import com.example.weatherapp.utils.showcurrentTime
 import com.example.weatherapp.utils.toString
+import com.example.weatherapp.viewmodels.TodayViewModel
 import com.example.weatherapp.viewmodels.WeeklyViewModel
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
@@ -35,22 +37,22 @@ import dagger.hilt.android.AndroidEntryPoint
 class WeeklyFragment : Fragment() {
 
     private lateinit var bnd: FragmentWeeklyBinding
-    private val viewModel: WeeklyViewModel by viewModels()
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-    private lateinit var lastLocation: Location
-    private var lastLong = 0.00
-    private var lastLat = 0.00
+    private val viewModel: TodayViewModel by activityViewModels()
+//    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+//    private lateinit var lastLocation: Location
+//    private var lastLong = 0.00
+//    private var lastLat = 0.00
     private val weatherAdapter = WeatherForDaysAdapter()
-
-    private lateinit var locationCallback: LocationCallback
-
-    private lateinit var locationRequest: LocationRequest
-    private var locationUpdateState = false
-
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
-        private const val REQUEST_CHECK_SETTINGS = 2
-    }
+//
+//    private lateinit var locationCallback: LocationCallback
+//
+//    private lateinit var locationRequest: LocationRequest
+//    private var locationUpdateState = false
+//
+//    companion object {
+//        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+//        private const val REQUEST_CHECK_SETTINGS = 2
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -58,23 +60,23 @@ class WeeklyFragment : Fragment() {
     ): View? {
         bnd = FragmentWeeklyBinding.inflate(layoutInflater, container, false)
 
-        fusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(requireActivity())
+//        fusedLocationProviderClient =
+//            LocationServices.getFusedLocationProviderClient(requireActivity())
 
         return bnd.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(p0: LocationResult) {
-                super.onLocationResult(p0)
-                lastLocation = p0.lastLocation
-                lastLat = lastLocation.latitude.format(2).toDouble()
-                lastLong = lastLocation.longitude.format(2).toDouble()
+//        locationCallback = object : LocationCallback() {
+//            override fun onLocationResult(p0: LocationResult) {
+//                super.onLocationResult(p0)
+//                lastLocation = p0.lastLocation
+//                lastLat = lastLocation.latitude.format(2).toDouble()
+//                lastLong = lastLocation.longitude.format(2).toDouble()
 //                viewModel.actionSearch(lastLat, lastLong)
-            }
-        }
-        createLocationRequest()
+//            }
+//        }
+//        createLocationRequest()
         setUpViews()
         setUpViewModels()
     }
@@ -83,8 +85,8 @@ class WeeklyFragment : Fragment() {
 
 
     private fun setUpViewModels() {
-        viewModel.actionSearch(6.45,3.65)
-        viewModel.weeklyFetchResponse.observe(viewLifecycleOwner) { result ->
+//        viewModel.actionSearch(6.45,3.65)
+        viewModel.todayFetchResponse.observe(viewLifecycleOwner) { result ->
             when (result) {
                 is Result.Error -> {}
                 is Result.Loading -> {
@@ -115,73 +117,73 @@ class WeeklyFragment : Fragment() {
         bnd.dateTv.text = currentDate
     }
 
-    private fun startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE
-            )
-            return
-        }
-        fusedLocationProviderClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            Looper.getMainLooper()
-        )
-    }
-
-    private fun createLocationRequest() {
-        locationRequest = LocationRequest()
-        locationRequest.interval = 10000
-        locationRequest.fastestInterval = 5000
-        locationRequest.priority =
-            PRIORITY_HIGH_ACCURACY
-        val builder = LocationSettingsRequest.Builder()
-            .addLocationRequest(locationRequest)
-        val client = LocationServices.getSettingsClient(requireActivity())
-        val task = client.checkLocationSettings(builder.build())
-        task.addOnSuccessListener {
-            locationUpdateState = true
-            startLocationUpdates()
-        }
-        task.addOnFailureListener { e ->
-            if (e is ResolvableApiException) {
-                try {
-                    e.startResolutionForResult(
-                        requireActivity(),
-                        REQUEST_CHECK_SETTINGS
-                    )
-                } catch (sendEx: IntentSender.SendIntentException) {
-                }
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CHECK_SETTINGS) {
-            if (resultCode == Activity.RESULT_OK) {
-                locationUpdateState = true
-                startLocationUpdates()
-            }
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if (!locationUpdateState) {
-            startLocationUpdates()
-        }
-    }
+//    private fun startLocationUpdates() {
+//        if (ActivityCompat.checkSelfPermission(
+//                requireContext(),
+//                android.Manifest.permission.ACCESS_FINE_LOCATION
+//            ) != PackageManager.PERMISSION_GRANTED
+//        ) {
+//            ActivityCompat.requestPermissions(
+//                requireActivity(),
+//                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+//                LOCATION_PERMISSION_REQUEST_CODE
+//            )
+//            return
+//        }
+//        fusedLocationProviderClient.requestLocationUpdates(
+//            locationRequest,
+//            locationCallback,
+//            Looper.getMainLooper()
+//        )
+//    }
+//
+//    private fun createLocationRequest() {
+//        locationRequest = LocationRequest()
+//        locationRequest.interval = 10000
+//        locationRequest.fastestInterval = 5000
+//        locationRequest.priority =
+//            PRIORITY_HIGH_ACCURACY
+//        val builder = LocationSettingsRequest.Builder()
+//            .addLocationRequest(locationRequest)
+//        val client = LocationServices.getSettingsClient(requireActivity())
+//        val task = client.checkLocationSettings(builder.build())
+//        task.addOnSuccessListener {
+//            locationUpdateState = true
+//            startLocationUpdates()
+//        }
+//        task.addOnFailureListener { e ->
+//            if (e is ResolvableApiException) {
+//                try {
+//                    e.startResolutionForResult(
+//                        requireActivity(),
+//                        REQUEST_CHECK_SETTINGS
+//                    )
+//                } catch (sendEx: IntentSender.SendIntentException) {
+//                }
+//            }
+//        }
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == REQUEST_CHECK_SETTINGS) {
+//            if (resultCode == Activity.RESULT_OK) {
+//                locationUpdateState = true
+//                startLocationUpdates()
+//            }
+//        }
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        if (!locationUpdateState) {
+//            startLocationUpdates()
+//        }
+//    }
 
 }
